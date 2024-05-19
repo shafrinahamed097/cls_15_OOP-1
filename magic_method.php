05a92730f3114cd9d47e759deed367e162525574
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-19 13:56:28 +0600

    Added json convert in magic method

--- magic_method.php
+++ magic_method.php
@@ -1,20 +1,31 @@
 <?php
-
-
-class Person{
+class Person {
     private $name;
     private $age;
 
-    function __construct($name, $age){
+    function __construct($name, $age) {
         $this->name = $name;
         $this->age = $age;
     }
 
-    function __get($key){
-        return $this->$key;
+    function introduce() {
+        echo "My name is {$this->name} and I am {$this->age} years old\n";
+    }
+
+    function __toString(){
+        // return "Name: {$this->name}, Age: {$this->age}";
+        // convert to json
+        return json_encode(["name" => $this->name, "age" => $this->age]);
+    }
+
+  function __get($key) {  // Made public
+        if ($key == "name" || $key == "age") {
+            return $this->$key;
+        }
+        throw new Exception("Invalid Property");
     }
 }
 
+
 $p1 = new Person("Shakib", 27);
-echo $p1->name;
-echo $p1->age;
+echo $p1 ; // Added newline for readability
