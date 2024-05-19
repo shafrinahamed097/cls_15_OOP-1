103258855801970289de365d9cab14ac6b7148cb
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-05-19 16:10:46 +0600

    Object clone

--- object_clone.php
+++ object_clone.php
@@ -1,17 +1,15 @@
 <?php 
 
 class Person{
-    public function __construct(public $name){}
+    public function __construct(public $name, public $age){}
 
     function introduce(){
-        echo "My name is {$this->name}" ."\n";
+        echo "My name is {$this->name}, and I'm {$this->age} years old" ."\n";
     }
 }
 
-$p1 = new Person('John');
-$p2 = $p1;
-
-$p2->name="Jane";
-
-$p1->introduce() ;
+$p1 = new Person("John", 30);
+$p2 = clone $p1;
+$p2->name="David";
+$p1->introduce();
 $p2->introduce();
